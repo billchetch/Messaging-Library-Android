d1ca8f030ac258df90be8a1593e0b81920b0e040
billchetch <bill@chetch.net>
2020-09-06 16:31:57 +0800

    mss

--- messaging/src/main/java/net/chetch/messaging/filters/CommandResponseFilter.java
+++ messaging/src/main/java/net/chetch/messaging/filters/CommandResponseFilter.java
@@ -1,11 +1,32 @@
 package net.chetch.messaging.filters;
 
+import net.chetch.messaging.Message;
 import net.chetch.messaging.MessageFilter;
 import net.chetch.messaging.MessageType;
 
 abstract public class CommandResponseFilter extends MessageFilter {
 
-    public CommandResponseFilter(String sender) {
+    String originalCommand;
+
+    public CommandResponseFilter(String sender, String originalCommand) {
         super(sender, MessageType.COMMAND_RESPONSE);
+
+        this.originalCommand = originalCommand;
+    }
+
+    public CommandResponseFilter(String sender) {
+        this(sender, null);
+    }
+
+    @Override
+    protected boolean matches(Message message) {
+        boolean matched = super.matches(message);
+        if(!matched)return matched;
+
+        if(originalCommand != null){
+            return originalCommand.equalsIgnoreCase(message.getString("OriginalCommand"));
+        } else {
+            return matched;
+        }
     }
 }
